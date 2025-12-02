d86e3b358e9371589aaa9339c2a72142049409bd
Poojithamodala <poojithamodala26@gmail.com>
2025-12-02 15:55:58 +0530

    Updated

--- notification-service/src/main/java/com/flightapp/service/KafkaConsumerConfig.java
+++ notification-service/src/main/java/com/flightapp/service/KafkaConsumerConfig.java
@@ -1,38 +1,38 @@
 package com.flightapp.service;
 
-import com.flightapp.messaging.BookingEvent;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.flightapp.messaging.BookingEvent;
 
 @Configuration
 public class KafkaConsumerConfig {
 
-    @Bean
-    public ConsumerFactory<String, BookingEvent> bookingEventConsumerFactory() {
-        JsonDeserializer<BookingEvent> deserializer = new JsonDeserializer<>(BookingEvent.class);
-        deserializer.addTrustedPackages("*");
-        Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-service");
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer.getClass());
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), deserializer);
-    }
+	@Bean
+	public ConsumerFactory<String, BookingEvent> bookingEventConsumerFactory() {
+		JsonDeserializer<BookingEvent> deserializer = new JsonDeserializer<>(BookingEvent.class);
+		deserializer.addTrustedPackages("*");
+		Map<String, Object> config = new HashMap<>();
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		config.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-service");
+		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer.getClass());
+		return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), deserializer);
+	}
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, BookingEvent> bookingEventKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, BookingEvent> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(bookingEventConsumerFactory());
-        return factory;
-    }
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, BookingEvent> bookingEventKafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, BookingEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(bookingEventConsumerFactory());
+		return factory;
+	}
 }
