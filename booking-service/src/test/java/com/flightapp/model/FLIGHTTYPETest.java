f180d15ff9b832e294a4f3617064f217d45c6f31
Poojithamodala <poojithamodala26@gmail.com>
2025-12-02 15:53:04 +0530

    Updated

--- booking-service/src/test/java/com/flightapp/model/FLIGHTTYPETest.java
+++ booking-service/src/test/java/com/flightapp/model/FLIGHTTYPETest.java
@@ -1,19 +1,15 @@
 package com.flightapp.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class FLIGHTTYPETest {
 
-    @Test
-    void testEnumValues() {
-        // Check that ONE_WAY exists
-        assertEquals("ONE_WAY", FLIGHTTYPE.ONE_WAY.name());
-        // Check that ROUND_TRIP exists
-        assertEquals("ROUND_TRIP", FLIGHTTYPE.ROUND_TRIP.name());
-    }
+	@Test
+	void testEnumValues() {
 
-    
-   
+		assertEquals("ONE_WAY", FLIGHTTYPE.ONE_WAY.name());
+		assertEquals("ROUND_TRIP", FLIGHTTYPE.ROUND_TRIP.name());
+	}
 }
