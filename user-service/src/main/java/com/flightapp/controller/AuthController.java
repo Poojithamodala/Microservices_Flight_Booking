4ff42da8028c731ec8d4ace3fcaa6cf98d26877f
Poojithamodala <poojithamodala26@gmail.com>
2025-12-02 16:00:53 +0530

    Updated user service

--- user-service/src/main/java/com/flightapp/controller/AuthController.java
+++ user-service/src/main/java/com/flightapp/controller/AuthController.java
@@ -1,47 +1,35 @@
 package com.flightapp.controller;
 
+import com.flightapp.model.User;
+import com.flightapp.service.AuthService;
 import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import com.flightapp.model.User;
-import com.flightapp.service.AuthService;
-
-import jakarta.validation.Valid;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthService authService;
+	private final AuthService authService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
+	public AuthController(AuthService authService) {
+		this.authService = authService;
+	}
 
-    @PostMapping("/register")
+	@PostMapping("/register")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Mono<String> register(@RequestBody User user) {
-		 return authService.register(user)
-		            .map(savedUser -> "user created with id: " + savedUser.getId());
+		return authService.register(user).map(savedUser -> "user created with id: " + savedUser.getId());
 	}
-    
-    @PostMapping("/login")
-    public Mono<ResponseEntity<Map<String, String>>> login(@RequestBody User user) {
-        return authService.login(user.getEmail(), user.getPassword())
-                .map(sessionId ->
-                        ResponseEntity.ok().body(
-                                Map.of("sessionId", sessionId, "message", "Login successful")
-                        ))
-                .onErrorResume(ex ->
-                        Mono.just(
-                                ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                                        .body(Map.of("error", ex.getMessage()))
-                        )
-                );
-    }
+
+	@PostMapping("/login")
+	public Mono<ResponseEntity<Map<String, String>>> login(@RequestBody User user) {
+		return authService.login(user.getEmail(), user.getPassword()).map(
+				sessionId -> ResponseEntity.ok().body(Map.of("sessionId", sessionId, "message", "Login successful")))
+				.onErrorResume(ex -> Mono
+						.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", ex.getMessage()))));
+	}
 
 }
